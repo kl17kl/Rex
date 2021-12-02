fd12a380090ec27586b16893aef9e7bae17f6ff7
Jesse Masciarelli <46304793+jmasciarelli@users.noreply.github.com>
2021-12-01 21:04:43 -0500

    Tabs created
    proj ready

--- app/src/main/java/com/example/rex/MainActivity.java
+++ app/src/main/java/com/example/rex/MainActivity.java
@@ -4,11 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch (tab.getPosition()) {
+                    case 0:
+                        System.out.println("tab 1");
+                        break;
+                    case 1:
+                        System.out.println("tab 2");
+                        break;
+                    case 2:
+                        System.out.println("tab 3");
+                        break;
+                    default:
+                        System.out.println("Defaulted");
+                }
+            }
+                @Override
+                public void onTabUnselected (TabLayout.Tab tab){
+
+                }
+
+                @Override
+                public void onTabReselected (TabLayout.Tab tab){
+
+                }
+
+        });
     }
+
 }
