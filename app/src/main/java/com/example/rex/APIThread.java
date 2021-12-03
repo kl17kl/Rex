8de65ac0fb778960390ea284ff01ca6dbb53acae
Jesse Masciarelli <46304793+jmasciarelli@users.noreply.github.com>
2021-12-02 19:40:27 -0500

    APIThread class added
    minor formatting with global params

---
+++ app/src/main/java/com/example/rex/APIThread.java
@@ -0,0 +1,96 @@
+package com.example.rex;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class APIThread extends Thread {
+
+    private String q; //the input query (Drake, Matilda, etc.) - required
+    private String t; //the query type (music, movie, show) - optional
+    private int l; //the return limit (eg. return only 5 results) - optional
+    private int i; //1 for more info, 0 otherwise - optional
+
+    private JSONArray info, results;
+    private Boolean complete = false;
+    public APIThread (String query, String type, int limit, int info) {
+        q = query;
+        t = type;
+        l = limit;
+        i = info;
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            // Connecting to the API and performing a search with the input key
+            String stringUrl = "https://tastedive.com/api/similar?q="+ q;
+            if(t!=null) stringUrl+="&type="+t;
+            if(l!=20) stringUrl+="&limit="+l;
+            if(i!=0) stringUrl+="&info="+i;
+            stringUrl += "&k=426198-Recommen-80WFYS8W"; //api key appended to end
+
+            URL url = new URL(stringUrl);
+
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+            // Setting up the input stream and buffer readers
+            InputStream in = urlConnection.getInputStream();
+            InputStreamReader reader = new InputStreamReader(in);
+            BufferedReader br = new BufferedReader(reader);
+            StringBuffer result = new StringBuffer();
+
+            // Reading in the JSON results from the API
+            String line;
+            while ((line = br.readLine()) != null) {
+                result.append(line);
+            }
+            br.close();
+
+            System.out.print(result.toString());
+
+            // Converting results into a JSONObject
+            JSONObject json = new JSONObject(result.toString());
+            JSONObject similar = json.getJSONObject("Similar");
+            System.out.println("RESULTS: " + json);
+
+            // Saving the JSON's Info and Results data
+            info = similar.getJSONArray("Info");
+            results = similar.getJSONArray("Results");
+
+            /* Code: printing out values from JSONArray
+            for (int i=0; i<info.length(); i++) {
+                JSONObject rec = info.getJSONObject(i);
+                String name = rec.getString("Name");
+                String type = rec.getString("Type");
+                System.out.println(name+" "+type);
+            }*/
+
+            // Wait for main process to kill thread
+            complete = true;
+            while (true) {}
+
+        } catch (Exception e) {
+        }
+    }
+
+    public JSONArray getInfo() {
+        return this.info;
+    }
+
+    public JSONArray getResults() {
+        return this.results;
+    }
+
+    public Boolean isComplete() {
+        return this.complete;
+    }
+
+}
+
--- app/src/main/java/com/example/rex/MainActivity.java
+++ app/src/main/java/com/example/rex/MainActivity.java
@@ -2,11 +2,20 @@ package com.example.rex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.google.android.material.tabs.TabLayout;
 
+import org.json.JSONArray;
+
 public class MainActivity extends AppCompatActivity {
+
+    String queryType = "music"; //defaults
+    int queryLimit = 20;
+    int queryInfo = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +30,15 @@ public class MainActivity extends AppCompatActivity {
                 switch (tab.getPosition()) {
                     case 0:
                         System.out.println("kt peanut");
+                        queryType = "music";
                         break;
                     case 1:
                         System.out.println("tab 2");
+                        queryType = "movie";
                         break;
                     case 2:
                         System.out.println("tab 3");
+                        queryType = "show";
                         break;
                     default:
                         System.out.println("Defaulted");
@@ -43,6 +55,31 @@ public class MainActivity extends AppCompatActivity {
                 }
 
         });
+    }
+
+    /** doSearch executes when the search button is pressed. It takes the user input and uses
+     * the global variables to make an api call, and then handles the return.
+     * */
+
+    public void doSearch(View view) {
+        // Get the search query
+//        EditText textArea = (EditText)findViewById(R.id.searchEditText);
+//        String input = textArea.getText().toString();
+//
+//        // Start a thread to pull the API data
+//        APIThread thread = new APIThread(input,queryType,queryLimit,queryInfo);
+//        thread.start();
+//        while (!thread.isComplete()) {}
+//
+//        // Save the API data into JSONArrays
+//        JSONArray infoArray = thread.getInfo();
+//        JSONArray resultsArray = thread.getResults();
+//
+//        // Start new ExploreMMS activity with JSON data
+//        Intent intent = new Intent(ExploreRandoms.this, ExploreMMS.class);
+//        intent.putExtra("info", String.valueOf(infoArray));
+//        intent.putExtra("results", String.valueOf(resultsArray));
+//        startActivity(intent);
     }
 
 }
